01d8dc277950b27e95647bc04e1141d3cbe940f6
chenjian <com.me@foxmail.com>
2021-03-11 15:32:49 +0800

    spock 2.0-M4-groovy3.0

--- src/test/groovy/com/KtSpockSpec.groovy
+++ src/test/groovy/com/KtSpockSpec.groovy
@@ -4,6 +4,8 @@ package com
 import org.slf4j.LoggerFactory
 import spock.lang.Specification
 
+import java.time.LocalDateTime
+
 class KtSpockSpec extends Specification {
 
     def log = LoggerFactory.getLogger({}.getClass())
@@ -27,6 +29,8 @@ class KtSpockSpec extends Specification {
 
     def "test2"() {
         when:
+        println LocalDateTime.now()
+        println("test2")
         def res = 1 + 1
         then:
         res == 2
